ef23b9b9b8abd044ea2a16b89215a77d589c1133
MoralFuck <bital2509@gmail.com>
2020-01-14 10:54:10 +0300

    add: page

--- www/about/index.js
+++ www/about/index.js
@@ -3,7 +3,7 @@ let menu = document.getElementById('MenuList');
 let body = document.getElementById('body');
 
 burger.addEventListener('click',function () {
-    menu.classList.toggle('menu-list_active');
-    burger.classList.toggle('burger_active');
-    body.classList.toggle('lock');
+   menu.classList.toggle('menu-list_active');
+   burger.classList.toggle('burger_active');
+   body.classList.toggle('lock');
 });
