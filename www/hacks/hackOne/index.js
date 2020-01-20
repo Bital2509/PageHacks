31fe42066bbe036bf68cc3cc62b8b9db1581a65a
MoralFuck <bital2509@gmail.com>
2020-01-20 11:07:41 +0300

    add: page

--- www/hacks/hackOne/index.js
+++ www/hacks/hackOne/index.js
@@ -2,8 +2,22 @@ let burger = document.getElementById('Burger');
 let menu = document.getElementById('MenuList');
 let body = document.getElementById('body');
 
-burger.addEventListener('click',function () {
-   menu.classList.toggle('menu-list_active');
-   burger.classList.toggle('burger_active');
-   body.classList.toggle('lock');
+burger.addEventListener('click', function () {
+    menu.classList.toggle('menu-list_active');
+    burger.classList.toggle('burger_active');
+    body.classList.toggle('lock');
 });
+
+let offsetHeight = document.getElementById('imagesOne');
+console.log(offsetHeight.width);
+document.getElementById('left').onclick = sliderLeft;
+let left = 0;
+
+function sliderLeft() {
+    let polosa = document.getElementById('polosa');
+    left = left - 1280;
+    if (left < -3840) {
+        left = 0
+    }
+    polosa.style.left = left + 'px';
+}
