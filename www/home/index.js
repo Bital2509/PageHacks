f0958f4ff93393c30e1b02ef17b6667e77462df1
MoralFuck <bital2509@gmail.com>
2020-02-06 15:38:31 +0300

    add: document for block

--- www/home/index.js
+++ www/home/index.js
@@ -2,7 +2,7 @@ let burger = document.getElementById('Burger');
 let menu = document.getElementById('MenuList');
 let body = document.getElementById('body');
 
-burger.addEventListener('click',function () {
+burger.addEventListener('click', function () {
     menu.classList.toggle('menu-list_active');
     burger.classList.toggle('burger_active');
     body.classList.toggle('lock');
